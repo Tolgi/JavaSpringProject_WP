55819fd66719d52a7ce62515b10527fd275a0671
Tolgi <vvstojanova@gmail.com>
2020-03-09 22:05:38 +0100

    project init

---
+++ src/test/java/finki/ukim/mk/hospital_managment_system/HospitalManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.ukim.mk.hospital_managment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
